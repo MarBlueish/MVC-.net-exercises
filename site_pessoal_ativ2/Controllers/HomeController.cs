8c5ecc6735147357e9fe4ca6c7b78b46ab2e303f
MarBlueish <6958234@formacao.iefp.pt>
2024-02-22 17:04:24 +0000

    update

---
+++ site_pessoal_ativ2/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using site_pessoal.Models;
+using System.Diagnostics;
+
+namespace site_pessoal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult sobremim()
+        {
+            return View();
+        }
+
+        public IActionResult interesses()
+        {
+            return View();
+        }
+
+        public IActionResult formacao()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
